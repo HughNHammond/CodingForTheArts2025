da71738527e4b03bc285f40858f90d01cbb3e733
HughHammondRHUL <Hugh.Hammond@rhul.ac.uk>
2025-03-09 11:48:30 +0000

    more test

--- Merge Test/sketch.js
+++ Merge Test/sketch.js
@@ -2,7 +2,7 @@ function setup() {
     console.log('This is a console log!');
 
 
-    param1 = true;
+    param1 = false;
     console.log("Some parameters were just called");
     param2 = false;
 }
